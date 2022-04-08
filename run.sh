2f4a8fc09e135f931c9d8c9383af115beb71d784
Mats Sjöberg <mats.sjoberg@csc.fi>
2022-04-08 14:32:13 +0300

    run.sh to use NVME

--- run.sh
+++ run.sh
@@ -6,9 +6,13 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=32G
 #SBATCH --time=15
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:v100:1,nvme:10
 
 module purge
 module load pytorch
 
-srun python3 cifar10_cnn.py --data_path=/scratch/project_2001234/cifar-10-batches-py
+set -x
+
+tar xf /scratch/project_2001234/cifar-10-python.tar.gz -C $LOCAL_SCRATCH
+
+srun python3 cifar10_cnn.py --data_path=$LOCAL_SCRATCH/cifar-10-batches-py
